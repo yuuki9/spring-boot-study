c716b4845c16e11819f9324fc6366171f3eb8efc
yuuki <wlsghtjd23@gmail.com>
2023-02-16 22:33:48 +0900

    [번경사항] yml 파일 설정 추가

--- jpa-optimization/src/main/java/com/study/app/JpaOptimizationApplication.java
+++ jpa-optimization/src/main/java/com/study/app/JpaOptimizationApplication.java
@@ -3,10 +3,13 @@ package com.study.app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.time.Clock;
+
 @SpringBootApplication
 public class JpaOptimizationApplication {
-
+	
 	public static void main(String[] args) {
+		System.out.println("체크");
 		SpringApplication.run(JpaOptimizationApplication.class, args);
 	}
 
