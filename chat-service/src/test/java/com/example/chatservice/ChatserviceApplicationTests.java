0d0451b0f852a0960389513a2a89ef67b6dc7449
yuuki <wlsghtjd23@gmail.com>
2023-02-20 20:56:28 +0900

    chat service

---
+++ chat-service/src/test/java/com/example/chatservice/ChatserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.chatservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
