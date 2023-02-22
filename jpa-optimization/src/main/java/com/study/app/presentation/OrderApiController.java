f94c1b3c97bc5743073fe865866b03c77ea2eb52
진호성 <yuuki@newlayer.kr>
2023-02-22 17:59:38 +0900

    v2 v3 케이스 추가

--- jpa-optimization/src/main/java/com/study/app/presentation/OrderApiController.java
+++ jpa-optimization/src/main/java/com/study/app/presentation/OrderApiController.java
@@ -2,12 +2,14 @@ package com.study.app.presentation;
 
 
 import com.study.app.domain.order.Orders;
+import com.study.app.presentation.dto.OrderDto;
 import com.study.app.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @RestController
@@ -17,11 +19,57 @@ public class OrderApiController {
 
     /**
      * V1. 엔티티 직접 노출
-     * - 양방향 관계 문제 발생 -> @JsonIgnore
+     * - 엔티티가 변하면 API 스펙이 변한다.
+     * - 트랜잭션 안에서 지연 로딩 필요
+     * - 양방향 연관관계 문제
+     *
+     * V2. 엔티티를 조회해서 DTO로 변환(fetch join 사용 X)
+     * - 트랜잭션 안에서 지연 로딩 필요
+     *
+     * V3. 엔티티를 조회해서 DTO로 변환(fetch join 사용 O)
+     * - 페이징 시에는 N 부분을 포기해야함(대신에 batch fetch size? 옵션 주면 N -> 1 쿼리로 변경 가능)
+     *
+     * V4. JPA에서 DTO로 바로 조회, 컬렉션 N 조회 (1 + N Query)
+     * - 페이징 가능
+     *
+     * V5. JPA에서 DTO로 바로 조회, 컬렉션 1 조회 최적화 버전 (1 + 1 Query)
+     * - 페이징 가능
+     *
+     * V6. JPA에서 DTO로 바로 조회, 플랫 데이터(1Query) (1 Query)
+     * - 페이징 불가능
      */
+
     @GetMapping("/api/v1/orders")
     public List<Orders> ordersV1() {
         List<Orders> all = orderRepository.findAll();
+        for (Orders order : all) { // Lazy 강제 초기화
+            order.getMember().getName();
+            order.getDelivery().getAddress();
+            order.getOrderItems().stream()
+                    .forEach(o -> o.getItem().getName());
+        }
         return all;
+    }
+
+    /**
+     * V2.엔티티를 조회해서 DTO로 변환 (fetch join 사용 X)
+     * - 단점: 지연로딩으로 쿼리 N번 호출 (N+1문제 발생)
+     */
+    @GetMapping("/api/v2/orders")
+    public List<OrderDto> ordersV2() {
+        return orderRepository.findAll().stream()
+                .map(o -> new OrderDto(o))
+                .collect(Collectors.toList());
+    }
+
+    /**
+     * V3.엔티티를 조회해서 DTO로 변환 (fetch join 사용 O)
+     * - fetch join으로 쿼리 1번 호출
+     */
+    @GetMapping("/api/v3/orders")
+    public List<OrderDto> ordersV3() {
+        return orderRepository.findAllWithItem().stream()
+                .map(OrderDto::new)
+                .collect(Collectors.toList());
     }
 }
--- jpa-optimization/src/main/java/com/study/app/repository/OrderRepository.java
+++ jpa-optimization/src/main/java/com/study/app/repository/OrderRepository.java
@@ -21,5 +21,19 @@ public class OrderRepository {
         return entityManager.createQuery("select o from Orders o", Orders.class).getResultList();
     }
 
+    public List<Orders> findAllWithMemberDelivery() {
+        return entityManager.createQuery("select o from Order o " +
+                "join fetch o.member m " +
+                "join fetch o.delivery d", Orders.class)
+                .getResultList();
+    }
 
+    public List<Orders> findAllWithItem(){
+        return entityManager.createQuery("select distinct o from Orders o " +
+                "join fetch o.member m " +
+                "join fetch o.delivery d " +
+                "join fetch o.orderItems oi " +
+                "join fetch oi.item i", Orders.class)
+                .getResultList();
+    }
 }
